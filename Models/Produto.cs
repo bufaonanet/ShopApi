db2b19a66e379f6369ec4fbdc575b9d8685f929b
douglas souto de souza <douglas_bufao@yahoo.com.br>
2020-07-09 16:06:46 -0300

    Adição do Model Produto

---
+++ Models/Produto.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations; //para as validações
+
+namespace Shop.Models
+{
+    public class Produto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Esse campo é obrigatório")]
+        [MaxLength(60, ErrorMessage = "Esse campo deve conter de 3 a 60 caracteres")]
+        [MinLength(60, ErrorMessage = "Esse campo deve conter de 3 a 60 caracteres")]
+        public string Titulo { get; set; }
+
+        [MaxLength(1024, ErrorMessage = "Esse campo deve conter um máximo de 2014 caracteres")]
+        public string Descrica { get; set; }
+
+        [Required(ErrorMessage = "Esse campo é obrigatório")]
+        [Range(1, int.MaxValue, ErrorMessage = "Preço deve ser maior que zero")]
+        public decimal Preco { get; set; }
+
+        [Required(ErrorMessage = "Esse campo é obrigatório")]
+        [Range(1, int.MaxValue, ErrorMessage = "Categoria Inválida")]
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+
+
+    }
+
+}
