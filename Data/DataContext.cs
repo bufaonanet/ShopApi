2232cfdc889d24247c3ce3cd91d893ea244e0b0d
douglas souto de souza <douglas_bufao@yahoo.com.br>
2020-07-10 12:05:28 -0300

    Criando o data context e adicionando o EntityFrameworkcore a aplicaçao

---
+++ Data/DataContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Shop.Models;
+
+namespace Shop.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+
+    }
+
+}
