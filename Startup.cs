aee8ffc17d7696907216766104970d0bc8645049
douglas souto de souza <douglas_bufao@yahoo.com.br>
2020-07-14 12:33:12 -0300

    verificando erros

--- Startup.cs
+++ Startup.cs
@@ -70,7 +70,7 @@ namespace Shop
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            //if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
